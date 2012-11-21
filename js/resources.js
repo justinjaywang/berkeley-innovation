ba033094c239e17926c4a4d67f0b1b145eb588a5
justinjaywang <justinjaywang@gmail.com>
2012-11-21 00:55:47 -0800

    resource bundles

--- js/resources.js
+++ js/resources.js
@@ -1,16 +1,15 @@
 $(document).ready(function(){
-  $.getJSON('https://api-ssl.bitly.com/v3/user/link_history?access_token=05a8ff99765bdf9f5bd14d5cfcee8be033eeca39&callback=?', function(data){
-  	
+  
+  $.getJSON('https://api-ssl.bitly.com/v3/bundle/bundles_by_user?access_token=05a8ff99765bdf9f5bd14d5cfcee8be033eeca39&user=berkeleyinnovation&callback=?', function(data){
+    
   	$.each(data, function(key, item){
-      if (key=="data") {
-        $.each(item.link_history, function(i, link_obj){
-          // console.log(link_obj)
-          //           console.log(link_obj.title)
-          //           console.log(link_obj.long_url)
-          $('.bitly').prepend('<div class="four columns"><h3 class="resource"><a href="' + link_obj.long_url + '">' + link_obj.title + '</a></h3></div');
+  	  if (key=='data') {
+        $.each(item.bundles, function(i, bundle_obj){
+          $('.bitly').prepend('<div class="four columns"><a href="' + bundle_obj.bundle_link + '"><h3 class="bundle-title">' + bundle_obj.title + '</h3></a><p class="bundle-description">' + bundle_obj.description + '</p></div>')
         });
-      };
+  	  };
 		});
 		
   });
+  
 });
